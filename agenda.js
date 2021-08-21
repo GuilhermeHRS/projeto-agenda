a0848b4c3644367b956d684e1ba102fae659c81e
Guilherme Henrique Ramos dos Santos <Guilhermehsantos2412@gmail.com>
2021-08-21 16:06:21 -0300

    Adicionando o event listener nos botões, setando eles no arquivo 'agenda.js'

--- agenda.js
+++ agenda.js
@@ -1,9 +1,22 @@
-function novoEvento() {
-    var novoEvento = document.getElementById('novoEvento');
+var buttonNovoEvento = document.getElementById('buttonNovoEvento');
+var buttonCancelar   = document.getElementById('buttonCancelar');
+var novoEvento       = document.getElementById('novoEvento');
+var formNovoEvento   = document.getElementById('formNovoEvento');
+
+function mostrarNovoEvento() {
     novoEvento.classList.remove('d-none');
 }
 
-function cancelarEvento() {
-    var novoEvento = document.getElementById('novoEvento');
+function cancelarNovoEvento() {
     novoEvento.classList.add('d-none');
 }
+
+function salvarNovoEvento(event) {
+    console.log(event);
+    event.preventDefault();
+    console.log('Salvar Novo Evento');
+}
+
+buttonNovoEvento.addEventListener('click', mostrarNovoEvento);
+buttonCancelar.addEventListener('click', cancelarNovoEvento);
+formNovoEvento.addEventListener('submit', salvarNovoEvento);
